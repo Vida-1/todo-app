9b4b0a1b83dd1c6b6ef68cec1869c30b16910496
PROVIDA POWELL <PROVIDA_POWELL@YAHOO.COM>
2023-03-27 18:53:21 +0000

    pushing changes to github for accessing on different system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { MantineProvider, Text } from '@mantine/core';
 
 import Todo from './Components/Todo';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Todo />
+      <MantineProvider withGlobalStyles withNormalizeCSS
+        theme={{
+          fontFamily: 'Open Sans, sans serif',
+          spacing: { xs: '1rem', sm: '1.2rem', md: '1.8rem', lg: '2.2rem', x1: '2.8rem' } ,
+  }
+} 
+>
+        <Todo />
+      </MantineProvider>
     );
   }
 }
--- src/Components/Todo/index.jsx
+++ src/Components/Todo/index.jsx
@@ -1,7 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import useForm from '../../hooks/form';
-
+import useFormLocal from '../../hooks/form';
+import { MantineProvider, Button, TextInput, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
+// import { Slider, RangeSlider } from '@mantine/core';
 import { v4 as uuid } from 'uuid';
+
+
+// function MantineDemo() { //from: https://mantine.dev/form/use-form/
+//   const form = useForm({
+//     initialValues: {
+//       itemDetails: "Item Details",
+//       assigneeName: "Assignee Name"
+//     }
+//   });
+//   return (
+//     <Box maw = { 300} mx = "auto" >
+//       <form onSubmit={form.onSubmit((values) => console.log(values))}>
+//         <TextInput
+//           label="To Do Item"
+//           placeholder="Item Details"
+//           {...form.getInputProps('itemDetails')}
+//         />
+//         <Group position="right" mt="md">
+//           <Button type="submit">Submit2</Button>
+//         </Group>
+//       </form>
+//   </Box>
+//   );
+// }
 
 const Todo = () => {
 
@@ -10,7 +36,7 @@ const Todo = () => {
   });
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
-  const { handleChange, handleSubmit } = useForm(addItem, defaultValues);
+  const { handleChange, handleSubmit } = useFormLocal(addItem, defaultValues);
 
   function addItem(item) {
     item.id = uuid();
@@ -48,44 +74,82 @@ const Todo = () => {
 
   return (
     <>
-      <header data-testid="todo-header">
-        <h1 data-testid="todo-h1">To Do List: {incomplete} items pending</h1>
-      </header>
+      <MantineProvider>
+        <header data-testid="todo-header">
+          <h1 data-testid="todo-h1">To Do List: {incomplete} items pending</h1>
+        </header>
 
-      <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
+          <h2>Add To Do Item</h2>
+          <label>
+            <span>To Do Item</span>
+            <input
+              onChange={handleChange}
+              name="text"
+              type="text"
+              placeholder="Item Details"
+            />
+          </label>
+          <label>
+            <span>Assigned To</span>
+            <input
+              onChange={handleChange}
+              name="assignee"
+              type="text"
+              placeholder="Assignee Name"
+            />
+          </label>
 
-        <h2>Add To Do Item</h2>
+          <label>
+            <span>Difficulty</span>
+            <input
+              onChange={handleChange}
+              defaultValue={defaultValues.difficulty}
+              type="range"
+              min={1}
+              max={5}
+              name="difficulty"
+            />
+          </label>
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
+          {/* <label>
+            <Slider
+              color="indigo"
+              defaultValue={3}
+              const
+              marks={[
+                { value: 1, label: "1" },
+                { value: 2, label: "2" },
+                { value: 3, label: "3" },
+                { value: 4, label: "4" },
+                { value: 5, label: "5" },
+              ]}
+              onChange={handleChange}
+            ></Slider>
+          </label> */}
+          <label>
+            <Button variant="filled" color="indigo" type="submit">
+              Add Item
+            </Button>
+          </label>
+        </form>
 
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
-
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={defaultValues.difficulty} type="range" min={1} max={5} name="difficulty" />
-        </label>
-
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
-
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
-
+        {list.map((item) => (
+          <div key={item.id}>
+            <p>{item.text}</p>
+            <p>
+              <small>Assigned to: {item.assignee}</small>
+            </p>
+            <p>
+              <small>Difficulty: {item.difficulty}</small>
+            </p>
+            <div onClick={() => toggleComplete(item.id)}>
+              Complete: {item.complete.toString()}
+            </div>
+            <hr />
+          </div>
+        ))}
+      </MantineProvider>
     </>
   );
 };
